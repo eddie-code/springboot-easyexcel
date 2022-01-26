f56a42bcbd89b69b05317f07579a6b8e460e8c88
eddie-code <231071372@qq.com>
2022-01-26 14:40:22 +0800

    init project

---
+++ src/main/java/com/edcode/easyexcel/ExcelApplication.java
@@ -0,0 +1,18 @@
+package com.edcode.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author eddie.lee
+ * @date 2022-01-26 14:31
+ * @description
+ */
+@SpringBootApplication
+public class ExcelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExcelApplication.class, args);
+  }
+
+}
